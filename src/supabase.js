793c703802f997c0de36f7debe37f5cde8604233
Sudheer Kumar R <sudheerkumarr@Sudheers-MacBook-Pro.local>
2026-06-21 04:56:24 +0530

    fix supabase config

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://prwsbukoicqkjrscnxyv.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByd3NidWtvaWNxa2pyc2NueHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MTQ5MzQsImV4cCI6MjA5NzQ5MDkzNH0.mC97mVVF7HoJIFNXuvl162HH-EM_d6VGJ8AvmaThvec'
+  import.meta.env.VITE_SUPABASE_URL || 'https://prwsbukoicqkjrscnxyv.supabase.co',
+  import.meta.env.VITE_SUPABASE_KEY || 'your-full-anon-key-here'
 )
