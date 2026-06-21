b0827d6eeb211801df769c106c8d7c12fa3040ff
Sudheer Kumar R <sudheerkumarr@Sudheers-MacBook-Pro.local>
2026-06-21 06:03:31 +0530

    hardcode supabase keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL || 'https://prwsbukoicqkjrscnxyv.supabase.co',
-  import.meta.env.VITE_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByd3NidWtvaWNxa2pyc2NueHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MTQ5MzQsImV4cCI6MjA5NzQ5MDkzNH0.mC97mVVF7HoJIFNXuvl162HH-EM_d6VGJ8AvmaThvec'
+  'https://prwsbukoicqkjrscnxyv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByd3NidWtvaWNxa2pyc2NueHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5NzI0NTIsImV4cCI6MjA2NTU0ODQ1Mn0.Nxi0d8WtvahoDl4iymhoo_hJpvtj7L29IH-vm1OGNzE'
 )
