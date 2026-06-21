51c25b6d9e30bb59cb8caf1c0290fb4a4cfbc040
Sudheer Kumar R <sudheerkumarr@Sudheers-MacBook-Pro.local>
2026-06-21 06:52:24 +0530

    correct anon key

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://prwsbukoicqkjrscnxyv.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByd3NidWtvaWNxa2pyc2NueHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5NzI0NTIsImV4cCI6MjA2NTU0ODQ1Mn0.Nxi0d8WtvahoDl4iymhoo_hJpvtj7L29IH-vm1OGNzE'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByd3NidWtvaWNxa2pyc2NueHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MTQ5MzQsImV4cCI6MjA5NzQ5MDkzNH0.mC97mVVF7HoJIFNXuvl162HH-EM_d6VGJ8AvmaThvec'
 )
